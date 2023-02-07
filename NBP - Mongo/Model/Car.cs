e09b9611545ec4ab155825dd26385605e2a49d50
Rade-Stajkovic <radomir@elfak.com>
2023-02-07 20:24:08 +0100

    [BACK] - MODEL

---
+++ NBP - Mongo/Model/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBP___Mongo.Model
+{
+    public class Car
+    {
+        public Model Model { get; set; }
+
+        public Mark Mark { get; set; }
+
+        public  String ExteriorColor { get; set; }
+
+        public String InteriorColor { get; set; }
+
+        public String Drivetrain  { get; set; }
+
+        public int Year { get; set; }
+
+
+    }
+}
