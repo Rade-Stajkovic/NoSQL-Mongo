cb827126cb920962fe6b277a0601d5e4813f8117
senicjovan <senic.jovan@elfak.rs>
2023-02-07 21:10:29 +0100

    [BACK] - dodate klase RentCar i TestDrive

--- NBP - Mongo/Model/RentCar.cs
+++ NBP - Mongo/Model/RentCar.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,18 @@ namespace NBP___Mongo.Model
 {
     public class RentCar
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ID {  get; set; }
+
+        public DateTime OccupiedFrom { get; set; }  
+
+        public DateTime OccupiedUntill { get; set; }
+
+        public User User { get; set; }
+
+        public Car Car { get; set; }
+
+        public Dealer Dealer { get; set; }
     }
 }
