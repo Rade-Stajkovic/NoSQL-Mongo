05d1e8b5b0d71b909ffe860a724e76b17059610a
17859teodorapantic <teodora.pantic@elfak.rs>
2023-02-07 22:14:43 +0100

    BACK -Dealer class

--- NBP - Mongo/Model/Dealer.cs
+++ NBP - Mongo/Model/Dealer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,23 @@ namespace NBP___Mongo.Model
 {
     public class Dealer
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Location { get; set; }
+
+        public List<MongoDBRef> Cars { get; set; }
+        
+
+        public Dealer()
+        {
+            Cars = new List<MongoDBRef>();
+            
+        }
+
     }
 }
