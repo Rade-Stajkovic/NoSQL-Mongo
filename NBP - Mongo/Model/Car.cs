fae311ddcb47101b04c3182ac37175085fc391f9
17859teodorapantic <teodora.pantic@elfak.rs>
2023-02-07 22:05:20 +0100

    BACK - model

--- NBP - Mongo/Model/Car.cs
+++ NBP - Mongo/Model/Car.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace NBP___Mongo.Model
 {
     public class Car
@@ -18,6 +18,8 @@ namespace NBP___Mongo.Model
         public String Drivetrain  { get; set; }
 
         public int Year { get; set; }
+
+        public MongoDBRef Dealer{ get; set; }
 
 
     }
