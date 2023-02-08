351c0be722c3788d3a6be4f3598a04374afc7d07
senicjovan <senic.jovan@elfak.rs>
2023-02-08 16:47:28 +0100

    [BACK] - RentCarController

--- NBP - Mongo/Controllers/RentCarController.cs
+++ NBP - Mongo/Controllers/RentCarController.cs
@@ -1,43 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDbContext;
+using NBP___Mongo.Model;
+using NBP___Mongo.Services;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace NBP___Mongo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class RentCarController : ControllerBase
     {
-        // GET: api/<RentCarController>
+        private readonly RentCarService _rentCarService;
+        public RentCarController(RentCarService service)
+        {
+            this._rentCarService = service;
+        }
+      
         [HttpGet]
-        public string Get()
+        [Route("GetDealersRentals/{DealerID}")]
+        public async Task<IActionResult> GetDealersRentals(string DealerID)
         {
-            return "value1";
+            List<RentCar> list = await _rentCarService.GetDealersRentals(DealerID);
+            return new JsonResult(list);
         }
 
-        // GET api/<RentCarController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<RentCarController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("MakeCarRental/{OccupiedFrom}/{OccupiedUntill}/{CarID}/{DealerID}/{UserID}")]
+        public async Task<IActionResult> MakeCarRental(DateTime OccupiedFrom, DateTime OccupiedUntill, string CarID, string DealerID, string UserID)
         {
+            bool status = await _rentCarService.MakeCarRental(OccupiedFrom, OccupiedUntill, CarID, DealerID, UserID);
+            if (status) return Ok("Succesfully created rental!");
+            return BadRequest("Error");
         }
 
-        // PUT api/<RentCarController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        [Route("AllowRental/{RentalID}")]
+        public async Task<IActionResult> AllowRental(string RentalID)
         {
+            bool status = await _rentCarService.AllowRental(RentalID);
+            if (status) return Ok("Succesfully allowed rental!");
+            return BadRequest("Error");
         }
 
-        // DELETE api/<RentCarController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpPut]
+        [Route("FrobidRental/{RentalID}")]
+        public async Task<IActionResult> FrobidRental(string RentalID)
         {
+            bool status = await _rentCarService.FrobidRental(RentalID);
+            if (status) return Ok("Succesfully allowed rental!");
+            return BadRequest("Error");
         }
+
+
     }
 }
