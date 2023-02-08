de0127352350c3e204f50783f656f28295813aae
17859teodorapantic <teodora.pantic@elfak.rs>
2023-02-08 16:03:41 +0100

    BACK - dealer,logovanje

--- NBP - Mongo/Controllers/DealerController.cs
+++ NBP - Mongo/Controllers/DealerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
+using NBP___Mongo.Model;
 using NBP___Mongo.Services;
 using System;
 using System.Collections.Generic;
@@ -23,17 +24,49 @@ namespace NBP___Mongo.Controllers
 
         [HttpPost]
 
-        public IActionResult CreateDealer(string username, string password, string name, string location, IEnumerable<ObjectId> carIds)
+        public async Task<IActionResult> CreateUser( string username, string password, string name, string location)
         {
             try
             {
-                dealerService.CreateDealer(username, password, name ,location,carIds);
-                return Ok("uspelo");
+                var result = await dealerService.CreateDealer( username, password, name, location);
+                if (result == "Uspešno kreiran korisnik.")
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest(result);
+                }
             }
             catch (Exception e)
             {
-                throw new Exception(e.Message);
+                return BadRequest(e.Message);
             }
         }
+
+
+
+
+        [HttpGet]
+        [Route("LogInDealer/{username}/{password}")]
+        public IActionResult LogInDealer(String username, String password)
+        {
+            try
+            {
+                Task<Dealer> res = dealerService.LogInDealer(username, password);
+                Dealer res1 = res.Result;
+                if (res1 == null)
+                {
+                    return BadRequest("Korisnik ne postoji ili ste pogresli parametre za prijavu");
+                }
+
+                return Ok(res1);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
     }
 }
